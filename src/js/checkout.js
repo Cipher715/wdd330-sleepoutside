bc762d38ecae6959ba5281f7527ce7ade8649f05
Cipher715 <atsushi.karino@gmail.com>
2024-11-30 16:42:00 +0900

    checkout

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,3 +1,8 @@
 import { loadHeaderFooter } from "./utils.mjs";
+import CheckoutProcess from "./checkoutProcess.mjs";
 
 loadHeaderFooter();
+
+const myCheckout = new CheckoutProcess("so-cart", "#order-summary");
+myCheckout.init();
+myCheckout.calculateOrdertotal();
